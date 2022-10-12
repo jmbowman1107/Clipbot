c6ee969875cdf88e54f1414707eb131a95a14b76
Jeff Bowman <jeff@rafflesnow.com>
2022-10-12 15:50:24 -0400

    grr

--- BanHateBot/AdvancedClip.cs
+++ BanHateBot/AdvancedClip.cs
@@ -49,7 +49,7 @@ namespace BanHateBot
                     {
                         TwitchChatClient.SendMessage(e.ChatMessage.Channel, $"Clip created successfully {clip.CreatedClips[0].EditUrl.Replace("/edit", string.Empty)}");
                         MostRecentClips[e.ChatMessage.Username] = (clip.CreatedClips[0].EditUrl.Replace("/edit", string.Empty), DateTime.UtcNow);
-                        if (canPerformAdvancedClip) TwitchChatClient.SendMessage(e.ChatMessage.Channel, $"{e.ChatMessage.DisplayName} you can submit this clip to NoobHunter for consideration by typing !clip noobhunter in chat.");
+                        if (canPerformAdvancedClip) TwitchChatClient.SendMessage(e.ChatMessage.Channel, $"{e.ChatMessage.DisplayName} you can submit this clip to NoobHunter for consideration by typing \"!clip noobhunter\" in chat.");
 
                     }
                     else
